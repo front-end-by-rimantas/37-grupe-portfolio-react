6c863b0e68ff2a96c8c6860cf96d27878962af9a
Creat2die <Jevgenij.s@inbox.lt>
2022-06-30 15:43:28 +0300

    JS CONNECTED

---
+++ js/components/features.js
@@ -0,0 +1,71 @@
+function features(selector, data){
+    // validuojame atejusius duomenis
+    const inputResult = isValidFeatureInput(selector, data);
+    if (inputResult !== true){
+        return console.error(inputResult);
+    }
+
+    //logika
+    const DOM = document.querySelector(selector);
+    if (!DOM){
+        return console.error('Nerastas elementas i kuri reikia sugeneruoti nauja turini');
+    }
+
+    let HTML = '';
+
+
+    for (const item of data) {
+        if (!isValidFeatureItem(item)){
+            continue;
+        }
+
+        console.log(item);
+        HTML += `<div class="col-12 col-md-6 col-lg-4 futures">
+                <i class="icon fa fa-${item.icon}"></i>
+                <h3 class="title">${item.title}</h3>
+                <p class="description">${item.description}</p>
+                </div>`;
+    }
+    
+    //validuojame rezultata
+    if(HTML === ''){
+        return console.error('Duomenyse nera korektiskos informacijos');   
+    }
+
+    //graziname rezultata
+    DOM.innerHTML = HTML;
+
+    return true;
+}
+
+
+function isValidFeatureInput(selector,data){
+    if (typeof selector !== 'string'
+        || selector === ''){
+            return 'Services funtion requires non-empty array of data'
+        }
+
+    return true;
+}
+
+function isValidFeatureItem(data) {
+    if (typeof data !== 'object'
+        || data === null
+        || Array.isArray(data)
+        || typeof data.icon !== 'string'
+        || !data.icon
+        || data.icon.length > 15
+        || typeof data.title !== 'string'
+        || !data.title
+        || data.title.length > 20
+        || typeof data.description !== 'string'
+        || !data.description
+        || data.description.length > 100
+        || Object.keys(data).length !== 3) {
+        return false;
+    }
+    return true;
+}
+
+
+export {features}
---
+++ js/data/featuresData.js
@@ -0,0 +1,34 @@
+const featuresData = [
+    {
+        icon: 'laptop',
+        title: 'Test',
+        description: 'Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry\'s standard dummy text',
+    },
+    {
+        icon: 'laptop',
+        title: 'Test',
+        description: 'Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry\'s standard dummy text',
+    },
+    {
+        icon: 'laptop',
+        title: 'Test',
+        description: 'Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry\'s standard dummy text',
+    },
+    {
+        icon: 'laptop',
+        title: 'Test',
+        description: 'Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry\'s standard dummy text',
+    },
+    {
+        icon: 'laptop',
+        title: 'Test',
+        description: 'Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry\'s standard dummy text',
+    },
+    {
+        icon: 'laptop',
+        title: 'Test',
+        description: 'Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry\'s standard dummy text',
+    },
+];
+
+export {featuresData};
