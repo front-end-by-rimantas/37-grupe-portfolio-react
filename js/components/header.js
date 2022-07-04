7b8cbb87019a3a50a1f558cfb7ee08897ad6ae59
Darijush Prokopovich <darijus.pc@gmail.com>
2022-07-04 13:40:23 +0300

    js for header  sticky restiiling done

--- js/components/header.js
+++ js/components/header.js
@@ -2,23 +2,21 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-    document.querySelectorAll("#header").className = "container head head-sticky";
-    document.querySelectorAll("#headcol").className = "col-12 navmenu";
-    document.querySelectorAll("#logo").src = "./img/header-logo-blue.png";
-        let nodes = document.querySelectorAll(".menu");
+    document.getElementById("header").className = "container head head-sticky";
+    document.getElementById("headcol").className = "col-12 navmenu";
+    document.getElementById("logo").src = "./img/header-logo-blue.png";
+        let nodes = document.querySelectorAll(".mainlinks");
         for (let i = 0; i < nodes.length; i++) {
         nodes[i].style.color = "#000";
         
       }
   } else {
-    document.querySelectorAll("#header").className = "container head head-fixed";
-    document.querySelectorAll("#headcol").className = "col-12 col-lg-10 navmenu";
-    document.querySelectorAll("#logo").src = "./img/header-logo-white.png"
-    document.querySelectorAll("#main-menu").className = "mainlinks";
-        let nodes = document.querySelectorAll("#main-menu");
+    document.getElementById("header").className = "container head head-fixed";
+    document.getElementById("headcol").className = "col-12 col-lg-10 navmenu";
+    document.getElementById("logo").src = "./img/header-logo-white.png";
+        let nodes = document.querySelectorAll(".mainlinks");
         for (let i = 0; i < nodes.length; i++) {
         nodes[i].style.color = "#fff";
-        
   }
 } 
 }
