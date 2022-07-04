9a0b46ab9ae3158e7f9237dfcf90d56f90938f8c
Darijush Prokopovich <darijus.pc@gmail.com>
2022-07-04 15:18:01 +0300

    hamburger lines added

--- js/components/header.js
+++ js/components/header.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+  if ((document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) && window.innerWidth > 786) {
     document.getElementById("header").className = "container head head-sticky";
     document.getElementById("headcol").className = "col-12 navmenu";
     document.getElementById("logo").src = "./img/header-logo-blue.png";
