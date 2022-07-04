0b4037ccb1f2473e4b73aede166927bc3470ce85
Darijush Prokopovich <darijus.pc@gmail.com>
2022-07-04 10:12:33 +0300

    media styling started

--- js/components/header.js
+++ js/components/header.js
@@ -5,18 +5,20 @@ function scrollFunction() {
     document.getElementById("header").className = "container head head-sticky";
     document.getElementById("headcol").className = "col-12 navmenu";
     document.getElementById("logo").src = "./img/header-logo-blue.png";
-    // document.getElementById("main-menu").className = "mainlinks-black";
-    function myFunction() {
-        let nodes = document.getElementsById("main-menu");
+        let nodes = document.querySelectorAll("#main-menu");
         for (let i = 0; i < nodes.length; i++) {
-          nodes[i].style.color = "red";
-        }
+        nodes[i].style.color = "#000";
+        
       }
   } else {
     document.getElementById("header").className = "container head head-fixed";
     document.getElementById("headcol").className = "col-12 col-lg-10 navmenu";
     document.getElementById("logo").src = "./img/header-logo-white.png"
     document.querySelectorAll("main-menu").className = "mainlinks";
-    // document.getElementById("main-menu").className = "mainlinks ";
+        let nodes = document.querySelectorAll("#main-menu");
+        for (let i = 0; i < nodes.length; i++) {
+        nodes[i].style.color = "#fff";
+        
+  }
 } 
 }
