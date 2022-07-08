0d314edb62b9a8c4d9e77ca6ac5ba18b09c892d8
dmitrij <dzimaskurb@gmail.com>
2022-07-08 12:39:38 +0300

    FAQ section

--- js/pages/home.js
+++ js/pages/home.js
@@ -39,17 +39,16 @@ import {faqData} from '../data/faqData.js';
 
 faq('#faq_block', faqData);
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+const acc = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
